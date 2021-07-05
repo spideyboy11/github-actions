b34ac3bb01a9102ec33e12153cefc24f910617fe
spideyboy11 <83084002+spideyboy11@users.noreply.github.com>
2021-07-05 21:54:51 +0530

    Update pullRequestAction.js

--- .github/workflows/pullRequestAction.js
+++ .github/workflows/pullRequestAction.js
@@ -1,32 +1,36 @@
-async function getPullRequest(source, target, state) {
-    var existingPr = github.pulls.list({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        state: state,
-        head: source,
-        base: target
-    });
-    return existingPr;
-}
 
-async function createPullRequest(title, source, target) {
-    try {
-        var newPr = await github.pulls.create({
-            title: title,
+module.exports = async ({github, context, core}) => {
+
+    async function getPull(source, target, state) {
+        var existingPr = github.pulls.list({
             owner: context.repo.owner,
             repo: context.repo.repo,
+            state: state,
             head: source,
             base: target
         });
-    } catch (error) {
-        console.log(error.errors);
+        return existingPr;
     }
-}
+    
+    async function createPull(title, source, target) {
+        try {
+            var newPr = await github.pulls.create({
+                title: title,
+                owner: context.repo.owner,
+                repo: context.repo.repo,
+                head: source,
+                base: target
+            });
+        } catch (error) {
+            console.log(error.errors);
+        }
+    }
 
-var existingPullRequest = await getPullRequest("master", "testing", "open")
-if (existingPullRequest.data.length) {
-    console.log("PR already present");
-}
-else {
-    await createPullRequest("Master -> Testing", "master", "testing")
+    var existingPr = await getPull("master", "testing", "open")
+    if (existingPr.data.length) {
+        console.log("PR already present");
+    }
+    else {
+        await createPull("Master -> Testing", "master", "testing")
+    }
 }
